b4d2c770c9eaba2056d2ad613f6dc973b2c34d1c
Artem <artem.kulchitsky@gmail.com>
2023-03-24 15:35:37 +0100

    fix footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,6 @@
 	<TheMain />
 	<TheFooter />
 </template>
-
-<style scoped lang="scss">
-
-</style>
-
 <script>
 import TheHeader from '../components/TheHeader.vue';
 import TheFooter from '../components/TheFooter.vue';
